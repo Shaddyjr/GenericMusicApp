84a2b04fb9d26e5dc2cd36a6287f0590ba140fc3
mahdiASC <mahdi@allstarcode.org>
2018-05-14 00:16:25 -0400

    adding activities

---
+++ app/src/main/java/com/example/asc_guest/genericmusicapp/GenreActivity.java
@@ -0,0 +1,13 @@
+package com.example.asc_guest.genericmusicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_and_genre);
+    }
+}
